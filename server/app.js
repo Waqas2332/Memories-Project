9e26ec2b767a293092b7f57582750500a912c3c0
Waqas <wmunir232@gmail.com>
2023-07-17 12:18:11 +0500

    Implemeneted dotenv

--- server/app.js
+++ server/app.js
@@ -4,38 +4,25 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import multer from "multer";
 import PostRoutes from "./routes/posts.js";
-import path from "path";
-
-// const fileStorage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "public/images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(
-//       null,
-//       file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-//     );
-//   },
-// });
+import dotenv from "dotenv";
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
-// app.use(multer({ storage: fileStorage }).single("selectedFile"));
 app.use("/posts", PostRoutes);
-
-const CONNECTION_URL =
-  "mongodb+srv://wmunir232:wmunir232@task-manager.sf6auvh.mongodb.net/memories";
 
 const PORT = process.env.PORT || 5000;
 
 // { useNewUrlParser: true, useUnifiedTopology: true } These are optional but helps in removing unnecessary warnings from console
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
